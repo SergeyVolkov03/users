93b23cb408faf7a3d28aba903c9dd5e27eab11bf
Sergey Volkov <serzhvolkov03@gmail.com>
2025-06-05 10:49:34 +0300

    feat: add cors options

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors();
+  const corsOptions = {
+    origin: '*',
+    credentials: true,
+    optionSuccessStatus: 200,
+  };
+  app.use(cors(corsOptions));
   await app.listen(process.env.PORT ?? 3000);
   console.log(`Server is running on localhost:${process.env.PORT}`);
 }
